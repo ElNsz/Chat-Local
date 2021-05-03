6c4468ad685705682cc6d8f43015cc0c0565a809
Ellivelton Narciso <ellivelton.souza@gmail.com>
2021-05-03 04:13:09 -0300

    Alterado href inicial para index.php.

--- index.php
+++ index.php
@@ -9,15 +9,15 @@
 <body>
     <div class="botao-salas">
         <a href="./Chat1/index.php">Sala 1</a>
-        <a href="./Chat2/chat.php">Sala 2</a>
-        <a href="./Chat3/chat.php">Sala 3</a>
-        <a href="./Chat4/chat.php">Sala 4</a>
-        <a href="./Chat5/chat.php">Sala 5</a>
-        <a href="./Chat6/chat.php">Sala 6</a>
-        <a href="./Chat7/chat.php">Sala 7</a>
-        <a href="./Chat8/chat.php">Sala 8</a>
-        <a href="./Chat9/chat.php">Sala 9</a>
-        <a href="./Chat10/chat.php">Sala 10</a>
+        <a href="./Chat2/index.php">Sala 2</a>
+        <a href="./Chat3/index.php">Sala 3</a>
+        <a href="./Chat4/index.php">Sala 4</a>
+        <a href="./Chat5/index.php">Sala 5</a>
+        <a href="./Chat6/index.php">Sala 6</a>
+        <a href="./Chat7/index.php">Sala 7</a>
+        <a href="./Chat8/index.php">Sala 8</a>
+        <a href="./Chat9/index.php">Sala 9</a>
+        <a href="./Chat10/index.php">Sala 10</a>
     </div>
 </body>
 </html>
